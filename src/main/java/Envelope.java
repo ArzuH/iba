cb4d1b7331af82c9576a64f0295ee945eb4c8773
User <arzuhlee1@gmail.com>
2019-10-11 21:48:15 +0400

    envelope-homework

--- src/main/java/Envelope.java
+++ src/main/java/Envelope.java
@@ -3,7 +3,7 @@ public class Envelope {
     public static void main(String[] args) {
         int width = 20;
         int height = 8;
-        double tan = ((double)width) / height;
+        double tan = ((double)width) / (height-1);
 
         // vertical loop
         for (int i = 0; i < height; i++) {
@@ -14,8 +14,8 @@ public class Envelope {
                         || i == height - 1 // last horizontal line
                         || j == 0          // left vertical line
                         || j == width - 1  // right vertical line
-                        || j == (int)(i * tan)
-                        || (width-j-1)==(int)(i*tan)
+                        || j == (int)(i * tan)&&(j<width/2)
+                        || (width-j-2)==(int)(i*tan) && (i<height/2)
                 ) {
                     System.out.print("*");
                 } else {
